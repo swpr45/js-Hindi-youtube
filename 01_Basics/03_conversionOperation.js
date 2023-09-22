d49e78e99f20e8a181dab932fd3f577629772ca7
Swapnil  Kuwar <83110537+swpr45@users.noreply.github.com>
2023-09-22 10:29:29 +0000

    ConversionOperator

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "33"
+
+// console.log(typeof(33))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+/* //conversion to number
+* "33" => 33
+   ""  => 0
+   "23ab" => NaN
+   true  => 1
+   false => 0
+   null => 0
+   undefined => undefined
+ */
+
+//   let isLoggedIn = 2
+//   let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+  /*
+  * 1(any non-zero number) => true
+    0 => false
+    "" => false
+    "dd"(any non-empty string) => true
+    null => false
+    */
+   let someNumber = 33
+   let stringNumber = String(someNumber)
+   console.log(stringNumber);
+   console.log(typeof(stringNumber))
+   
+
