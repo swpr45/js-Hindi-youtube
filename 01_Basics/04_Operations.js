274f9200b7a53535f55c4863e7039d314e223f47
Swapnil  Kuwar <83110537+swpr45@users.noreply.github.com>
2023-10-04 11:46:54 +0000

    mathematical operations

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,20 @@
+// console.log(1+1);
+// console.log(20*39);
+// console.log(2/0);
+// console.log(12%2);
+// let aNumber = 12
+// let aString = "12";
+// let result = aNumber + aString;
+// console.log(result);
+console.log("3"+3);
+console.log(3+"3");
+console.log(2+2+"6");
+console.log(2+"2"+6);
+console.log(null + 1);
+//console.log(null + undifined);
+console.log(true+1+"1");
+console.log(false + null);
+console.log(""+null);
+console.log(0+null);
+console.log(null * 3)
+console.log(("*")*4)
